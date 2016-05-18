55a989fe0850243e42517f87f134b5532c1db76a
Tom Waddell <tom@intelligentediting.com>
2016-05-18 15:05:39 +0100

    Added Range Search example

--- OfficeAddInSandboxWeb/App/Home/Home.js
+++ OfficeAddInSandboxWeb/App/Home/Home.js
@@ -8,20 +8,68 @@
         $(document).ready(function () {
             app.initialize();
 
-            $('#get-data-from-selection').click(getDataFromSelection);
+            $('#searchBtn').click(onSearchClick);
         });
     };
 
-    // Reads data from current document selection and displays a notification
-    function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    app.showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    app.showNotification('Error:', result.error.message);
-                }
-            }
-        );
-    }
+    var searchRanges = [];
+
+    function onSearchClick() {
+        cleanUp(searchRanges);
+        runSearch("Video")
+            .then(function(results) {
+                searchRanges = results;
+                displaySearchResults(searchRanges);
+            });
+    };
+
+    function runSearch(textToFind) {
+        var items = [];
+        return Word.run(function (context) {
+            var options = Word.SearchOptions.newObject(context);
+            options.matchWildCards = false;
+
+            var rangesFind = context.document.body.search(textToFind, options);
+            context.load(rangesFind, 'text,');
+            return context.sync()
+                .then(function () {
+                    $.each(rangesFind.items, function (i) {
+                            items.push(rangesFind.items[i]);
+                            context.trackedObjects.add(rangesFind.items[i]);
+                    });
+                    return context.sync();
+                });
+        })
+        .then(function () {
+            return items;
+        });
+    };
+
+    function displaySearchResults(ranges) {
+        var resultsList = $('#searchResults');
+        resultsList.html('');
+        $.each(ranges, function(i) {
+            $('<li/>')
+                .text(ranges[i].text)
+                .click({range: ranges[i]}, selectRange)
+                .appendTo(resultsList);
+        });
+    };
+
+    function selectRange(event) {
+        var range = event.data.range;
+        range.select();
+        range.context.sync()
+        .catch(function(error) {
+                console.log(error);
+            });
+    };
+
+    function cleanUp(ranges) {
+        $.each(ranges, function(i) {
+            ranges[i].context.trackedObjects.remove(ranges[i]);
+            ranges[i].context.sync();
+        });
+    };
+
 })();
